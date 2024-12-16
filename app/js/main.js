1c2836523930f14fa0c066eb4b99e2527752a2ae
Laithanator <144480666+Laithanator@users.noreply.github.com>
2024-12-16 14:02:15 -0500

    h5reah5eh

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,42 @@
-const getData = async () => {
+async function createNewDeck() {
   try {
     const response = await fetch(
       "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
     );
     const data = await response.json();
-    const id = data.deck_id;
-    response = await fetch(
-      `https://deckofcardsapi.com/api/deck/${id}/draw/?count=2`
-    );
-    return response;
+    return data.deck_id;
   } catch (error) {
-    error("error fetching");
+    console.error("error creating new deck");
   }
-};
+}
 
-getData();
+async function drawCards(deckId, pullNum) {
+  try {
+    const response = await fetch(
+      `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${pullNum}`
+    );
+    return await response.json();
+  } catch (error) {
+    console.error("error drawing cards");
+  }
+}
+async function newGame() {
+  const id = await createNewDeck();
+  const dealer = await dealerHand();
+  const player = [];
+}
+async function dealerHand(id) {
+  const cards = await drawCards(id, 2);
+  let blackJack;
+  for (let i = 0; i < cards.length; i++)
+    (cards) => {
+      cards[i].value == "ACE" ||
+      cards[i].value == "KING" ||
+      cards[i].value == "QUEEN" ||
+      cards[i].value == "JACK"
+        ? (blackJack = true)
+        : (blackjack = false);
+    };
+  return blackJack;
+}
+console.log(await dealerHand("i1y36ts23k8g"));
